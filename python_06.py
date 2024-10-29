56bb2de6cd27ad46c28c84a3db6e206fcfed2c50
kadir-zeren <k4dzeren@gmail.com>
2024-10-29 12:35:59 +0300

    first commit

---
+++ python_06.py
@@ -0,0 +1,13 @@
+list_1 = ['h', 'a', 'p', 'p' , 'y']
+word = 'happy'
+list_2 = list(word)
+print(list_1)
+print(list_2)
+
+listem = ['a', 'b' , ' c' , 'd']
+type(listem)
+
+word = 'happy'
+list(word)
+
+list('a b c')
