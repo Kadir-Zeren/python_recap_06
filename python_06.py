1edaa9232203aab72b26879599bb0bf64c502fde
kadir-zeren <k4dzeren@gmail.com>
2024-10-29 13:38:16 +0300

    practice

--- python_06.py
+++ python_06.py
@@ -11,3 +11,312 @@ word = 'happy'
 list(word)
 
 list('a b c')
+
+country = ['USA', 'Brasil', 'UK', 'Germany', 'Turkey', 'New Zealand']
+print(country)
+
+string_1 = 'I quit smoking'
+new_list_1 = list(string_1) # we created multi element list
+print(new_list_1)
+new_list_2 = [string_1] # this is a single element list
+print(new_list_2)
+
+text = 'Clarusway'
+list(text)
+[text]
+
+mixed_list = [11, 'Joseph', False, 3.14, None, [1, 2, 3]]
+
+my_list = ['Joseph', 'Clarusway', 2020]
+new_list1 = list(my_list) # what will be the output?
+new_list2 = [my_list]# what will be the output?
+print(new_list1)
+print(len(new_list1))# what is the length of the variable?
+print(new_list2)
+print(len(new_list2))
+
+listem = [1,2,3]
+len(listem)
+list(listem)
+yeni = [listem]
+yeni
+len(yeni)
+
+a = [[1,2,3], ['ali' , 'ayse' ], [True, False]]
+len(a)
+list(a)
+liste_a = [a]
+liste_a
+len(liste_a)
+
+text = "2023's perfect"
+list(text)
+[text]
+
+my_list1 = ["2023's perfect"]
+my_list2 = list("2023's perfect")
+print(my_list1)
+print(my_list2)
+
+city = ['Addis Ababa', 'Istanbul', 'New York' , 'Seoul' , 'Stockholm' , 'Sydney' ]
+print(len(city))
+
+list('1234')
+listem = [[1,2,3], 'bir string', 7.23, True ]
+len(listem)
+
+empty_list_1 = []
+empty_list_2 = list()
+
+bos1 = []
+len(bos1)
+
+bos2 = list()
+len(bos2)
+
+numbers = [1, 4, 7]
+numbers. append(9)
+numbers. append('9')
+numbers. append( ['a' , 'b', 'c'])
+len(numbers)
+print(numbers)
+
+empty_list = []
+empty_list.append(6666)
+empty_list. append('Multiverse')
+empty_list. append([0])
+print(empty_list)
+
+city = ['New York' , 'London' , 'Istanbul' , 'Seoul' , 'Sydney' ]
+city. append('Addis Ababa' )
+print(city)
+
+numbers = []
+numbers.append(1)
+numbers. append(2)
+numbers. append(3)
+numbers. append(4)
+print(numbers)
+
+numbers = [1, 4, 7]
+numbers.insert(2, 9)
+print(numbers)
+numbers.insert(2, 6)
+print (numbers)
+
+city = ['New York', 'London', 'Istanbul', 'Seoul', 'Sydney', 'Addis Ababa' ]
+city. insert(2, 'Stockholm' )
+print(city)
+
+listem = ['a', 'b', 'c', 'd']
+listem.insert(3,12)
+listem
+
+numbers = [1, 2, 3, 4]
+numbers.insert(4, 5)
+print(numbers)
+
+listem = [1,2,3,4]
+listem.insert(-1,5)
+listem
+
+listem = [1,2,3,4]
+listem
+
+listem.insert(4,5)
+listem
+
+numbers = [1, 4, 7, 9]
+numbers.remove(7)
+print(numbers)
+
+city = ['New York', 'London' , 'Stockholm' , 'Istanbul', 'Seoul' , 'Sydney' , 'Addis Ababa' ]
+city. remove( 'London' )
+print(city) # we have deleted 'London'
+
+numbers
+numbers.remove(9)
+numbers
+numbers
+
+numbers
+
+listem = [1,2,3,4,2,3,4,3] 
+listem.remove(2)
+listem
+listem.pop() 
+listem
+listem.pop(4)
+listem 
+
+listem = ['a', 'b', 'c', 'd']
+listem. remove('c')
+listem
+
+listem.pop()
+
+listem.pop(1)
+
+listem
+
+a = [1, 4, 7, 9, '9', ['a', 'b', 'c']]
+a.pop()
+a
+a. clear()
+a
+
+numbers = [4, 1, 9, 7]
+numbers. sort()
+print (numbers)
+
+listem = [2,7,3,0,5,7] 
+listem.sort()
+listem
+
+listem.sort(reverse=True)
+listem
+
+isimler = ['veli', 'ali', 'ayse', 'kemal']
+isimler. sort()
+isimler
+
+isimler.append( 'Zeki')
+isimler 
+
+isimler. sort()
+isimler
+
+ord('A')
+ord('Z')
+ord('a')
+chr(97)
+
+city = [ 'New York' , 'Stockholm', 'Istanbul', 'Seoul', 'Sydney', 'Addis Ababa' ]
+city.sort() # lists the items in alphabetical order
+print(city)
+
+list_1 = ['one', 'four', 'nine' ]
+list_2 = [' *- ', '@', 'False' ]
+list_3 = [True, False]
+list_4 = [[3], [44], [-12]]
+list_5 = [[1, 3], [44, 0], [-12, 1]]
+
+list_1 = ['one', 'four', 'nine']
+list_1.sort()
+list_1 
+
+list_2 = [' *- ', '@', 'False']
+list_2.sort()
+list_2
+
+ord('*') 
+ord('@')
+ord('F')
+
+print('Clarus' + """Way""")
+
+word = 'Clarus'
+word += '\tWay'
+print(word)
+
+city = 'Chicago'
+print(city[ ::- 1])
+
+city = 'Chicago'
+print (city[-1])
+
+fruit = 'apple'
+vegetable = 'spinach'
+print('I ate {} and {} ' .format(fruit, vegetable))
+
+fruit1 = 'Banana'
+fruit2 = 'Peach'
+print(2*fruit1 + '+' + 3*fruit2)
+
+Fruit = 'apple'
+vegetable = 'spinach'
+output = f"We bought {fruit} and {vegetable}"
+print(output)
+
+fruit = 'Banana'
+print('I ate {} . format(fruit)')
+
+veg = "Onion"
+print ("I love {} and {veg}" . format ("Oneo", veg="Apple"))
+
+city = "Chicago"
+print (city [ -2: - 6 :- 1])
+
+print( 'Clarus'+ """Way""")
+
+list_3 = [True, False]
+list_3.sort()
+list_3
+
+list_4 = [[3], [44], [-12]]
+list_4. sort()
+list_4
+list_5 = [[1, 3], [44, 0], [-12, 1]]
+list_5.sort()
+list_5
+
+city = ['Addis Ababa' , 'Istanbul', 'New York' , 'Seoul' , 'Stockholm' , 'Sydney' ]
+print(len(city))
+
+list_1 = ['one', 'four', 'nine' ]
+list_2 = [' *- ', 4, 'False', 0]
+list_3 = [True, False]
+
+isimler = ['veli', 'ali', 'ayse', 'kemal']
+sorted(isimler)
+isimler
+isimler = sorted(isimler)
+isimler
+
+isimler = ['veli', 'ali', 'ayse', 'kemal']
+sorted(isimler, reverse=True)
+isimler
+
+isimler.sort()
+isimler
+
+4+5
+4.5 + 3.2
+'abc' + 'cdef'
+
+[1,2,3] + ['a', 'b', 'c']
+[ 'a', 'b', 'c'] + [1,2,3]
+[ ' a' , ' b' , ' c' ] *3
+
+fishes = ['tuna', 'squid', 'seabass' ]
+
+fruit = ['Apple', 'Orange', 'Banana' ]
+fruit [1]
+
+word = ['h', 'a', 'p', 'p', 'y']
+print (word[1])
+
+colors = ['red', 'purple' , 'blue', 'yellow' , 'green' ]
+print(colors[2]) # If we start at zero,
+# the second element will be 'blue'.
+
+city = ['New York', 'London', 'Istanbul', 'Seoul', 'Sydney' ]
+city_list = []
+city_list. append(city) # we have created a nested list
+print(city_list)
+
+city_list = [['New York', 'London' , 'Istanbul' , 'Seoul' , 'Sydney' ]]
+print(city_list[0]) # access to first and only element
+
+city_list = [["New York", "London", "Istanbul", "Seoul", "Sydney"]]
+len(city_list)
+
+city_list[0]
+
+city_list = ['New York', 'London' , 'Istanbul' , 'Seoul' , 'Sydney' ]
+print(city_list[0]) # access to first and only element
+
+city_list = [ ['New York', 'London' , 'Istanbul' , 'Seoul' , 'Sydney' ]]
+print(city_list[0][2])
+
+city_list[0][2][3]
